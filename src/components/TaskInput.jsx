fcf442273c7aadb53aa81184f0d2efec135518a6
Manikandan <manikandan.thedev@gmail.com>
2025-09-26 09:46:00 +0530

    Small changes in shadow effect

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -8,7 +8,8 @@ export default function TaskInput({ newList, setNewList, edit, handleAddorUpdate
     return (
         <>
             <div className="task-input-container">
-                <input className="task-input" value={newList} onChange={handleChange} type="text" placeholder='Enter Your Task . . . . .' />
+                <input className="task-input" value={newList} onChange={handleChange} placeholder="Enter Your Task . . . . ." type="text" />
+
                 <button className="task-btn" onClick={handleAddorUpdate}>
                     {edit !== null ? "Update" : "Add Task"}</button>
             </div>
